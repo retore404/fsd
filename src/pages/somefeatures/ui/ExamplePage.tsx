cd5bdb5c8e0a238bca6fcc335df452f34591ea39
retore404 <44674058+retore404@users.noreply.github.com>
2025-06-22 17:32:59 +0900

    fix config

--- src/pages/somefeatures/ui/ExamplePage.tsx
+++ src/pages/somefeatures/ui/ExamplePage.tsx
@@ -3,11 +3,11 @@
 //import { ExampleButton } from "../../../shared/ui/ExampleButton" // This is NG(relative import from private api)
 // absolute
 //import { ExampleButton } from "src/shared/ui" // This is OK.
-//import { ExampleButton } from "src/shared/ui/ExampleButton" // I want this to be lint error.
+import { ExampleButton } from "src/shared/ui/ExampleButton" // I want this to be lint error.
 
 // alias
 //import { ExampleButton } from "@/shared/ui" // This is OK(import with alias from public api)
-import { ExampleButton } from "@/shared/ui/ExampleButton" // I want this to be lint error.
+//import { ExampleButton } from "@/shared/ui/ExampleButton" // I want this to be lint error.
 
 
 export const ExamplePage = () => {
