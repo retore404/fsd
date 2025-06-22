a53d2a4e39db463a42e0b39ec159db2f74e2cbb9
retore404 <44674058+retore404@users.noreply.github.com>
2025-06-22 16:18:58 +0900

    add example

--- src/pages/somefeatures/ui/ExamplePage.tsx
+++ src/pages/somefeatures/ui/ExamplePage.tsx
@@ -1,5 +1,14 @@
-import { ExampleButton } from "../../../shared/ui/" // This is OK
-//import { ExampleButton } from "../../../shared/ui/ExampleButton" // This is NG
+// relative
+//import { ExampleButton } from "../../../shared/ui/" // This is OK(relative import from public api)
+//import { ExampleButton } from "../../../shared/ui/ExampleButton" // This is NG(relative import from private api)
+// absolute
+//import { ExampleButton } from "src/shared/ui" // This is OK.
+//import { ExampleButton } from "src/shared/ui/ExampleButton" // I want this to be lint error.
+
+// alias
+//import { ExampleButton } from "@/shared/ui" // This is OK(import with alias from public api)
+import { ExampleButton } from "@/shared/ui/ExampleButton" // I want this to be lint error.
+
 
 export const ExamplePage = () => {
     return (<div><ExampleButton /></div>)
